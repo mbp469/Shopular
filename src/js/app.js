976118f11cf4746c101d48f03a74adc8af801926
Marian Powell <marianpowell@gmail.com>
2016-10-26 21:13:59 -0400

    trying to re-structure to make editable.

--- src/js/app.js
+++ src/js/app.js
@@ -1,114 +1,28 @@
-(function() {
-    'use strict';
 
     angular
-    .module('inventory', [])
-    .controller('InventoryController', function($scope) {
-      this.allItems =
-        [
-          {
-            id: 2957,
-            name: "widget",
-            price: 32,
-            quantity: 203,
-            color: "red",
-            discount: 31
-          },
-          {
-            id: 89274,
-            name: "golf club",
-            price: 98,
-            quantity: 10,
-            color: "black",
-            discount: 0
-          },
-          {
-            id: 64,
-            name: "iPhone",
-            price: 499,
-            quantity: 2,
-            color: "white",
-            discount: 0
-          },
-          {
-            id: 87363,
-            name: "bonzai tree",
-            price: 76,
-            quantity: 2,
-            color: "green",
-            discount: 0
-          },
-          {
-            id: 1736,
-            name: "towel",
-            price: 55,
-            quantity: 30,
-            color: "brown",
-            discount: 10
-          },
-          {
-            id: 4836,
-            name: "dog bed",
-            price: 99,
-            quantity: 10,
-            color: "beige",
-            discount: 50
-          },
-          {
-            id: 829,
-            name: "waste basket",
-            price: 15,
-            quantity: 40,
-            color: "silver",
-            discount: 0
-          },
-          {
-            id: 46,
-            name: "guitar",
-            price: 899,
-            quantity: 0,
-            color: "blue",
-            discount: 150
-          },
-          {
-            id: 17456,
-            name: "matcha tea",
-            price: 42,
-            quantity: 4,
-            color: "green",
-            discount: 11
-          },
-          {
-            id: 3292,
-            name: "enlightenment",
-            price: 99999,
-            quantity: 1,
-            color: "red",
-            discount: 0
-          },
-          {
-            id: 533,
-            name: "eggs",
-            price: 5,
-            quantity: 12,
-            color: "brown",
-            discount: 1
-          },
-          {
-            id: 683,
-            name: "pillow",
-            price: 27,
-            quantity: 10,
-            color: "black",
-            discount: 12
-          }
-        ]
+    .module('inventory', ['ngRoute'])
+
+    .config(function($routeProvider) {
+      $routeProvider
+      .when("/", {
+        templateUrl:"views/main.html"
+      })
+      .when("/edit", {
+        templateUrl:"views/edit.html"
+      });
+    })
+
+    .controller('InventoryController', function($scope, dataService) {
+
+      dataService.getItems(function(response){
+        $scope.allItems = response.data;
+      });
+
       $scope.tax = .0575;
       $scope.saleImg = "src/images/sale.png";
       this.isOnSale = function(item){
         return item.discount > 0;
-      }
-
+      };
 
       this.adjustPrice = function(item){
         var adjustedPrice = item.price - item.discount;
@@ -116,7 +30,10 @@
         adjustedPrice += taxAmount;
         return adjustedPrice.toFixed(2);
       }
+    })
+    .service('dataService',function($http){
+      this.getItems = function(callback) {
+        $http.get('src/mock/items.json')
+        .then(callback)
+      }
     });
-
-
-})();
