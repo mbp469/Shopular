5e0a9b361b32596a9cb2673844024fb6d0b7a583
Marian Powell <marianpowell@gmail.com>
2016-10-27 14:11:57 -0400

    localStorage working

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
     angular
-    .module('inventory', ['ngRoute'])
+    .module('inventory', ['ngRoute','LocalStorageModule'])
 
     .config(function($routeProvider) {
       $routeProvider
@@ -13,11 +13,25 @@
       });
     })
 
-    .controller('InventoryController', function($scope,dataService) {
-      dataService.getItems(function(res){
-        $scope.allItems = res.data;
+    .controller('InventoryController', function($scope,dataService,localStorageService) {
+
+      dataService.getItemsFromLocalStorage(function() {
       });
 
+      dataService.getItems(function(res){
+        $scope.allItems = dataService.getItemsFromLocalStorage() || res.data;
+      });
+
+      $scope.deleteItem = function(item, index) {
+        dataService.deleteItem(item);
+        $scope.allItems.splice(index, 1);
+        localStorageService.set('localStorageItems', $scope.allItems);
+      };
+      $scope.addItem = function(item) {
+        dataService.addItem(item);
+        $scope.allItems.unshift(item);
+        localStorageService.set('localStorageItems', $scope.allItems);
+      };
       $scope.tax = .0575;
       $scope.saleImg = "src/images/sale.png";
       this.isOnSale = function(item){
@@ -31,9 +45,29 @@
         return adjustedPrice.toFixed(2);
       };
 
-
     })
-    .service('dataService',function($http) {
+    .service('dataService',function($http,localStorageService) {
+
+      this.storeItems = function(){
+        localStorageService.set('localStorageItems', $scope.allItems);
+      };
+
+      this.getItemsFromLocalStorage = function(){
+        return localStorageService.get('localStorageItems');
+      };
+
       this.getItems = function(callback) {
-      $http.get('src/mock/items.json').then(callback)};
+        $http.get('src/mock/items.json').then(callback);
+      };
+
+      this.deleteItem = function(item){
+        alert("Deleted " + item.name + " from inventory.");
+      };
+
+      this.addItem = function(item) {
+        localStorageService.set('localStorageItems')
+        return item;
+      };
+
+
     });
